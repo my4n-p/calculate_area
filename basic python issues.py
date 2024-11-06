d7038b76236b81d3153c0ee9ac0ea5e24ebfab10
my4n-p <30250519@blackpool.ac.uk>
2024-11-06 10:06:44 +0000

    Update basic python issues.py
    Added triangle area functionality

--- basic python issues.py
+++ basic python issues.py
@@ -1,6 +1,10 @@
 def calculate_area(length, width):
     area = length * width
     return area
+        
+def calculate_triangle_area(height, width):
+    area = height * width * 0.5 
+    return area
 
 length = input("Waffle stuff:\n")
 width = input("Waffle more stuff:\n")
@@ -11,4 +15,5 @@ try:
 except:
     length,width = 0,0
 
-print(calculate_area(length,width))
+print("Area is: ", calculate_area(length,width))
+print("Triangle are is: ", calculate_triangle_area(length,width))
